7d98c2656bfea98565a0a6a13a9a01bf0cf15872
NiloticNarcotic <70078769+NiloticNarcotic@users.noreply.github.com>
2023-10-16 02:45:21 -0700

    Updated to AAT

---
+++ common/defines/TFN_defines.lua
@@ -0,0 +1,10 @@
+NDefines.NGame.START_DATE = "1981.1.1.12"
+NDefines.NGame.END_DATE = "2000.1.1.1"
+NDefines.NFocus.FOCUS_POINT_DAYS = 1						-- Each point takes a day
+
+NDefines_Graphics.NGraphics.BORDER_COLOR_SELECTION_STATE_G = 0.33
+NDefines_Graphics.NGraphics.BORDER_COLOR_SELECTION_PROVINCE_R = 1.9
+NDefines_Graphics.NGraphics.BORDER_COLOR_SELECTION_PROVINCE_G = 0.16
+NDefines_Graphics.NGraphics.BORDER_COLOR_SELECTION_PROVINCE_B = 0.16
+
+NDefines.NBuildings.INFRASTRUCTURE_RESOURCE_BONUS = 0.0
