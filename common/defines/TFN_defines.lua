54cf73fdd4c5c4493bd0c699f74a1f700863d495
NiloticNarcotic <70078769+NiloticNarcotic@users.noreply.github.com>
2024-01-12 05:28:03 -0800

    The Road Begins Here
    - Bunch of changes
    - New Tag

--- common/defines/TFN_defines.lua
+++ common/defines/TFN_defines.lua
@@ -2,9 +2,33 @@ NDefines.NGame.START_DATE = "1981.1.1.12"
 NDefines.NGame.END_DATE = "2000.1.1.1"
 NDefines.NFocus.FOCUS_POINT_DAYS = 1						-- Each point takes a day
 
+--- Graphics ---
 NDefines_Graphics.NGraphics.BORDER_COLOR_SELECTION_STATE_G = 0.33
 NDefines_Graphics.NGraphics.BORDER_COLOR_SELECTION_PROVINCE_R = 1.9
 NDefines_Graphics.NGraphics.BORDER_COLOR_SELECTION_PROVINCE_G = 0.16
 NDefines_Graphics.NGraphics.BORDER_COLOR_SELECTION_PROVINCE_B = 0.16
 
+NDefines_Graphics.NGraphics.SUN_HEIGHT = 300
+NDefines_Graphics.NGraphics.SUN_HEIGHT_WATER = 900
+NDefines_Graphics.NGraphics.SUN_LATITUDE = 848
+NDefines_Graphics.NGraphics.SUN_INTENSITY = 0.65
+NDefines_Graphics.NGraphics.MOON_INTENSITY = 1.5;
+
+NDefines_Graphics.NGraphics.BLOOM_SCALE = 0
+NDefines_Graphics.NGraphics.BRIGHT_THRESHOLD = 0
+NDefines_Graphics.NGraphics.CUBEMAP_INTENSITY = 0
+
+-- NDefines_Graphics.NGraphics.MOON_DIFFUSE_COLOR = {1.0, 1.0, 1.0} -- HSV values
+-- NDefines_Graphics.NGraphics.SUN_DIFFUSE_COLOR = {1.0, 1.0, 1.0},
+
 NDefines.NBuildings.INFRASTRUCTURE_RESOURCE_BONUS = 0.0
+
+
+
+-- GAMEPLAY --
+NDefines.NCountry.BASE_RESEARCH_SLOTS = 1 -- World Stupid
+NDefines.NCountry.POPULATION_YEARLY_GROWTH_BASE = 0.035 -- People reproduce more now that the world has reached a population minimum
+NDefines.NCountry.WEEKLY_STABILITY_GAIN = -0.005 -- Things are still deteriorating, if left unchecked, your nation will be turned to chaos
+NDefines.NCountry.WAR_SUPPORT_TENSION_IMPACT = 0.0	-- World is fucked, why do we care?
+NDefines.NCountry.SCORCHED_EARTH_STATE_COST = 15 -- More expensive to scorch, more expensive to abandon
+
